0904e364d855ea00b12e2f2744515c319ed0a0bb
Pablo Cornehl <seinoxygen@hotmail.com>
2014-02-16 09:25:41 -0300

    Refactored the check function to be reused

--- jquery.validation.js
+++ jquery.validation.js
@@ -1,75 +1,98 @@
-(function($) {   
+(function($) {    
     var plugin = $(this);
     
 	$.fn.validate = function(options) {
-		// Update all configs
-		 var settings = $.extend({
+		
+		// Default config
+		var defaults = {
+			auto: false,
 			error: '#EB4847',
 			success: '#63C76A'
-		}, options);
+		};
+		
+		// Update all configs
+		var settings = $.extend(defaults, options);
+		
+		var error = false;
 		
 		// Check all fields
 		$(this).each(function() {
 			
-			// On key up check all rules of the field
+			if(settings.auto === true){
+				error = plugin['check']($(this), settings);
+			}
+			
 			$(this).keyup(function(e) {
 				e.preventDefault();
-				
-				// Explode rules				
-				var rules = $(this).data('rules').split('|');
-				
-				var error = false;
-				for (var i = 0; i < rules.length; i++) {
-					var rule = rules[i];
-					var regex = /(.*?)\[(.*?)\]/;
-										
-					var func = rule;
-					var options = '';
-					
-					// Check if arguments are enclosed in brackets			
-					var groups = regex.exec(rule);	
-					if(groups !== null){
-						func = groups[1];
-						options = groups[2];
-					}
-					
-					// If the function exists call it.			
-					if (typeof(plugin[func]) !== "undefined") {
-						error = plugin[func]($(this).val(), options);
-					}
-				}
-				
-				// Apply the correct css to the field.
-				if(error){
-					$(this).css('border-color', settings.error);
-				}
-				else{
-					$(this).css('border-color', settings.success);
-				}
+				error = plugin['check']($(this), settings);
 			});
 		});
-				
-		return;
+		
+		// Invert result !error = validation successful
+		return !error;
 	};
 	
 	/**
+	 * Run the rules in the field.
+	 * 
+ 	 * @param object value
+ 	 * @param object value
+	 */
+	plugin.check = function(element, settings) {
+		// Explode rules				
+		var rules = $(element).data('rules').split('|');
+				
+		var error = false;
+		for (var i = 0; i < rules.length; i++) {
+			var rule = rules[i];
+			var regex = /(.*?)\[(.*?)\]/;
+										
+			var func = rule;
+			var options = '';
+				
+			// Check if arguments are enclosed in brackets			
+			var groups = regex.exec(rule);	
+			if(groups !== null){
+				func = groups[1];
+				options = groups[2];
+			}
+					
+			// If the function exists call it.			
+			if (typeof(plugin[func]) !== "undefined") {
+				error = plugin[func]($(element).val(), options);
+			}
+		}
+				
+		// Apply the correct css to the field.
+		if(error){
+			$(element).css('border-color', settings.error);
+		}
+		else{
+			$(element).css('border-color', settings.success);
+		}
+		return error;
+	};
+		
+	/**
 	 * Checks if the field is filled.
+	 * 
  	 * @param string value
 	 */
 	plugin.required = function(value) {
-		if(value.length == 0){
+		if(value.length === 0){
 			return true;
 		}
 		return false;		
 	};
-	
+		
 	/**
 	 * Checks if the field is x characters long.
+	 * 
  	 * @param string value
  	 * @param integer value
 	 */
 	plugin.exactlength = function(value, options) {
-		if(value.length == options){
+		if(value.length === options){
 			return true;
 		}
 		return false;	
@@ -77,6 +100,7 @@
 	
 	/**
 	 * Checks if the field is at least x characters long.
+	 * 
  	 * @param string value
  	 * @param integer value
 	 */
@@ -101,6 +125,7 @@
 	
 	/**
 	 * Checks if the field only contains alpha characters.
+	 * 
  	 * @param string value
 	 */
 	plugin.alpha = function(value) {
@@ -110,6 +135,7 @@
 	
 	/**
 	 * Checks if the field only contains alpha-numeric characters.
+	 * 
  	 * @param string value
 	 */
 	plugin.alphanumeric = function(value) {
@@ -119,6 +145,7 @@
 		
 	/**
 	 * Checks if the field is numeric.
+	 * 
  	 * @param string value
 	 */
 	plugin.numeric = function(value) {
@@ -128,6 +155,7 @@
 	
 	/**
 	 * Checks if the numeric value is between two values.
+	 * 
  	 * @param string value
  	 * @param integer value
 	 */
@@ -141,5 +169,5 @@
 		}
 		return false;	
 	};
-	
+		
 }(jQuery));
