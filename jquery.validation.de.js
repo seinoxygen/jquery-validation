af44acdf7f28f92994e4bd809a680b46f09a30cf
Pablo Cornehl <seinoxygen@hotmail.com>
2014-03-05 14:42:08 -0300

    German translation

---
+++ jquery.validation.de.js
@@ -0,0 +1,21 @@
+var validation_lang = {
+	required: 'Das Feld %s ist erforderlich.',
+	nozero: 'Das Feld %s kann nicht Null sein.',
+	exactlength: 'Das Feld %s muß exakt %k zeichen lang sein.',
+	minlength: 'Das Feld %s muß mindestens %k Zeichen lang sein.',
+	maxlength: 'Das Feld %s muß weniger als %k Zeichen lang sein.',
+	alpha: 'Das Feld %s muß nur Buchstaben sein.',
+	alphanumeric: 'Das Feld %s muß nur Buchstaben und Zahlen sein.',
+	numeric: 'Das Feld %s muß ein numerischen Wert sein.',
+	integer: 'Das Feld %s muß eine ganze Zahl sein.',
+	natrual: 'Das Feld %s muß eine natürliche Zalh sein.',
+	decimal: 'Das Feld %s muß eine Dezimalzahl sein.',
+	between: 'Das Feld %s muß ein Wert zwischen %k und %n sein.',
+	matches: 'Die Felder %s und %k müssen übereinstimmen.',
+	email: 'Das Feld %s muß eine gültige email sein.',
+	url: 'Das Feld %s muß eine gültige url sein.',
+	ip: 'Das Feld %s muß eine gültige ip sein.',
+	time: 'Das Feld %s muß eine gültige Zeit sein.',
+	date: 'Das Feld %s muß ein gültiges datum sein.',	
+	cc: 'Das Feld %s ist nicht eine gültige Kreditkartennummer.'
+};
